847c0027ebf18e80e27fe73dffd83ab32d96e146
Einar Ingebrigtsen <einar@dolittle.com>
2013-01-20 16:00:26 +0100

    Chapter 4: Groups

--- SignalRChat.net/Program.cs
+++ SignalRChat.net/Program.cs
@@ -8,16 +8,41 @@ namespace SignalRChat.net
         static void Main(string[] args)
         {
             var hubConnection = new HubConnection("http://localhost:1599");
-
             var chat = hubConnection.CreateHubProxy("chat");
-            chat.On("addMessage", message => Console.WriteLine("Received : "+message));
+
+            var currentRoom = "Lobby";
+
+            chat.On("addMessage", (string room,string message) => {
+                if( room == currentRoom ) 
+                    Console.WriteLine("Received : " + message);
+            });
+
+            chat.On("addChatRoom", room => Console.WriteLine("Room added : "+room));
 
             hubConnection.Start().Wait();
+
+            Console.WriteLine("Connected");
+
+            chat.Invoke("Join", currentRoom).Wait();
 
             var line = string.Empty;
             while ((line = Console.ReadLine()) != null)
             {
-                chat.Invoke("Send", line).Wait();
+                if (line.StartsWith("/create"))
+                {
+                    var room = line.Substring("/create".Length + 1);
+                    chat.Invoke("CreateChatRoom", room);
+                }
+                if (line.StartsWith("/join"))
+                {
+                    var room = line.Substring("/join".Length + 1);
+                    chat.Invoke("Join", room).Wait();
+                    currentRoom = room;
+                }
+                else
+                {
+                    chat.Invoke("Send", currentRoom, line).Wait();
+                }
             }
         }
     }
---
+++ SignalRChat/ChatRooms.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace SignalRChat
+{
+    public class ChatRooms
+    {
+        static List<string> _rooms = new List<string>();
+
+        static ChatRooms()
+        {
+            _rooms.Add("Lobby");
+        }
+
+        public static void Add(string name)
+        {
+            _rooms.Add(name);
+        }
+
+        public static bool Exists(string name)
+        {
+            return _rooms.Contains(name);
+        }
+
+        public static IEnumerable<string> GetAll()
+        {
+            return _rooms;
+        }
+    }
+}
