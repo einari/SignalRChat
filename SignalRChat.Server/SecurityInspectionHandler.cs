1f91dcffba02eb7bdf92d4b013e3982e97e87c7b
Einar Ingebrigtsen <einar@dolittle.com>
2013-02-17 02:17:18 -0800

    Chapter 9: Owin

---
+++ SignalRChat.Server/SecurityInspectionHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Server
+{
+    public class SecurityInspectionHandler
+    {
+        Func<IDictionary<string, object>, Task> _appFunc;
+
+        public SecurityInspectionHandler(Func<IDictionary<string, object>, Task> appFunc)
+        {
+            _appFunc = appFunc;
+        }
+
+        public Task Invoke(IDictionary<string, object> environment)
+        {
+            environment["server.User"] =
+                new GenericPrincipal(
+                    new GenericIdentity("SomeCreator"), new[] { "Creator" });
+            return _appFunc.Invoke(environment);
+        }
+    }
+}
+
+
